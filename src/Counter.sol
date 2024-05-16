f0c874ca9227a2225d592dbb08c66b3ce4cedf32
milosdjurica <milosdjurica13@gmail.com>
2024-05-16 18:42:31 +0200

    arr and map test

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.23;
 
 contract Counter {
     uint256 public s_number;
     uint256 public s_number2;
     uint256 public constant NUMBER3 = 3;
+
+    uint256[] public arr;
+    mapping(uint256 => uint256) public map;
 
     function setNumber(uint256 _newNumber) public {
         s_number = _newNumber;
@@ -12,5 +15,21 @@ contract Counter {
 
     function increment() public {
         s_number++;
+    }
+
+    function pushArr(uint256 a) public {
+        arr.push(a);
+    }
+
+    function getArrElement(uint256 a) public view returns (uint256) {
+        return arr[a];
+    }
+
+    function getMapElement(uint256 a) public view returns (uint256) {
+        return map[a];
+    }
+
+    function addMap(uint256 a, uint256 b) public {
+        map[a] = b;
     }
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.23;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
@@ -15,6 +15,18 @@ contract CounterTest is Test {
     function test_Increment() public {
         counter.increment();
         assertEq(counter.s_number(), 1);
+    }
+
+    function test_ArrMap() public {
+        counter.pushArr(1111);
+        counter.pushArr(1111);
+        counter.pushArr(1111);
+
+        counter.addMap(0, 222);
+        counter.addMap(1, 222);
+        counter.addMap(2, 222);
+
+        assertEq(counter.getArrElement(2), counter.getMapElement(2));
     }
 
     function testFuzz_SetNumber(uint256 x) public {
